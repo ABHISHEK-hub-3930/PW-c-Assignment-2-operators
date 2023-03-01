98d23ceb508509321b61810b7a29a56a206db5d0
ABHISHEK KUMAR <112901841+ABHISHEK-hub-3930@users.noreply.github.com>
2023-03-01 21:57:05 +0530

    Add files via upload

---
+++ Operators/airthematicoperation.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+  float num1,num2;
+  cout<<"Enter first number ";
+  cin>>num1;
+  cout<<"Enter Second number ";
+  cin>>num2;
+  cout<<"sum= "<<num1+num2<<endl;
+  cout<<"differnce = "<<num1-num2<<endl;
+  cout<<"product = "<<num1*num2<<endl;
+  cout<<"division = "<<num1/num2<<endl;
+
+}
---
+++ Operators/girlsgettingA.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int boy,girls,total;
+    boy=17;
+    total=(85*45)/100;
+    girls=total-boy;
+    cout<<"How many girls are getting A = "<<girls<<endl;
+}
